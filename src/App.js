3d9ce7382d152d1e0fb8b421275c4a86dec575a0
Niharika I <niharikacpl@gmail.com>
2024-10-28 22:54:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './utilities/Navbar';
 import WhatsAppIcon from './utilities/WhatsAppIcon';
+import { ThemeProvider } from "@material-tailwind/react"; // Ensure you have the correct import
+import VIP from './components.jsx/mainpage/yuyiii_vip';
 
 function App() {
   return (
+    <ThemeProvider>
     <div className="App">
+      
       <Navbar />
+      <VIP/>
       <WhatsAppIcon/>
-    </div>
+    </div></ThemeProvider>
   );
 }
 
---
+++ src/components.jsx/mainpage/yuyiii_vip.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const VIP = () => {
+  const videoId = "7dC2IzA1a6s"; // YouTube video ID
+
+  return (
+    <div className="flex flex-col items-center">
+      {/* Video Embed */}
+      <div className="relative w-full">
+        <iframe
+          className="w-full h-[56.25vw] max-h-[600px] shadow-lg"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube Video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+
+        {/* Overlay for Thumbnail */}
+        <div className="absolute top-20 left-20 bg-black bg-opacity-50 text-white text-sm font-bold p-2 rounded">
+          Meet Sumedh! AKA the BirdMan
+        </div>
+
+        <div className="absolute bottom-5 right-5 bg-black bg-opacity-50 text-white text-sm font-bold p-2 rounded">
+        Experience the comfort of local companionship
+        </div>
+
+      </div>
+
+      {/* Caption Below Video */}
+    </div>
+  );
+};
+
+export default VIP;
